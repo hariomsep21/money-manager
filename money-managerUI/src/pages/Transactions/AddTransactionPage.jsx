418080feea7c08dc80381acae208417a56897051
Hariom Srivastava <hariom.srivastav@eatechnologies.net>
2025-12-21 16:37:30 +0530

    Working on UI for Trans

--- money-managerUI/src/pages/Transactions/AddTransactionPage.jsx
+++ money-managerUI/src/pages/Transactions/AddTransactionPage.jsx
@@ -18,7 +18,7 @@ const AddTransactionPage = () => {
                 style={{
                     position: 'absolute',
                     top: '0',
-                    left: '0',
+                    right: '0',
                     padding: '0.5rem',
                     background: 'transparent',
                     border: 'none',
